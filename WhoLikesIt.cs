731c9e0db83e94c7ba61fc428b51bc3bee38f7fc
Salavat <45713278+13cyberpunk02@users.noreply.github.com>
2019-09-19 12:18:24 +0300

    who likes it kata solution

---
+++ WhoLikesIt.cs
@@ -0,0 +1,22 @@
+using System;
+
+public static class Kata
+{
+  public static string Likes(string[] name)
+  {
+    int number = name.Length - 2;
+    if(name.Length == 0)
+      return "no one likes this";
+    else
+      if(name.Length <= 3)
+      {
+        if(name.Length == 1)
+          return $"{name[0]} likes this";
+        if(name.Length == 2)
+          return $"{name[0]} and {name[1]} like this";
+        if(name.Length == 3)
+          return $"{name[0]}, {name[1]} and {name[2]} like this";
+      }
+        return $"{name[0]}, {name[1]} and {number} others like this";      
+  }
+}
