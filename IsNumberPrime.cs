6ee963a7910ed069e2f6118a021526f482ff9237
Salavat <45713278+13cyberpunk02@users.noreply.github.com>
2019-09-19 12:29:14 +0300

    solution of kata

---
+++ IsNumberPrime.cs
@@ -0,0 +1,13 @@
+public static class Kata
+{
+  public static bool IsPrime(int n)
+  {
+    if(n <= 1) return false;
+    for(int i = 2; i * i <= n; i++)
+    {
+      if(n % i == 0)
+        return false;
+    }
+    return true;
+  }
+}
